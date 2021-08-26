fceae6fadb7f41401b2edd1119196513f24feab4
Devin Arena <devin0309@yahoo.com>
2021-08-26 10:18:08 -0400

    dropdown bar

--- app-tracker/src/components/navbar.js
+++ app-tracker/src/components/navbar.js
@@ -1,12 +1,18 @@
 import './navbar.css'
 
+/**
+ * Builds the Navbar for application, containing a dropdown
+ * menu with a list of operations for the user.
+ * 
+ * @returns JSX for a navbar element
+ */
 const Navbar = () => {
     return (
         <div className="navbar">
-            <h1>InternshipTracker</h1>
+            <h1>AppTracker</h1>
             <div className="dropdown">
                 <h1>Menu</h1>
-                <ul>
+                <ul className="menu">
                     <li><a href="">Test</a></li>
                     <li><a href="">Test</a></li>
                     <li><a href="">Test</a></li>
